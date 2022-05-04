07bdc4ca85e4bc96e20ceff214f63d0480792c02
unknown <annieto@unal.edu.co>
2022-05-03 20:31:26 -0500

    Pregunta #2

--- preguntas.py
+++ preguntas.py
@@ -60,19 +60,19 @@ def pregunta_02():
     )
 
     # Imprima las dimensiones del DataFrame
-    print(____.____)
+    print(df.shape)
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    print(____)
+    print(df['life'].corr(df['fertility']).round(decimals=4))
 
     # Imprima la media de la columna `life` con 4 decimales.
-    print(____)
+    print(df['life'].mean().round(decimals=4))
 
     # Imprima el tipo de dato de la columna `fertility`.
-    print(____)
+    print(df.dtypes['fertility'])
 
     # Imprima la correlación entre las columnas `GDP` y `life` con 4 decimales.
-    print(____)
+    print(df['GDP'].corr(df['life']).round(decimals=4))
 
 
 def pregunta_03():
